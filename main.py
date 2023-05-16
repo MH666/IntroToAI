19d9a0eba09c6c2a4dd5b85b94b45c43c1698806
Min He Ng <minhe0611@gmail.com>
2023-05-16 17:34:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+def parse_input(filename):
+    with open(filename, 'r') as f:
+        content = [line.strip() for line in f.read().split('\n')]
 
+    tell_index = content.index('TELL')
+    ask_index = content.index('ASK')
+
+    knowledge_base = content[tell_index+1:ask_index]
+    query = content[ask_index+1:][0]
+
+    # Split the knowledge base into individual propositions
+    knowledge_base = [item.strip() for sublist in knowledge_base for item in sublist.split(';') if item]
+
+    return knowledge_base, query
